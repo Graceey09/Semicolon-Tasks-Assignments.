dee42a5090208dc293cdbd60ce256844054965ab
Graceey09 <gracemaduekwe95@gmail.com>
2023-10-19 11:09:59 +0100

    code updates

--- src/Jonathan_Task/IsAnagram.java
+++ src/Jonathan_Task/IsAnagram.java
@@ -1,2 +1,33 @@
-package Jonathan_Task;public class IsAnagram {
+package Jonathan_Task;
+
+import java.util.Arrays;
+
+public class IsAnagram {
+    public static void main(String[] args) {
+        String result = String.valueOf(checkIsAnagram("chair", "chair"));
+        System.out.println(result);
+    }
+
+    public static boolean checkIsAnagram(String word1, String word2) {
+        char[] words = word1.toCharArray();
+        char[] letters = word2.toCharArray();
+        char[] finalWord = sort(words);
+        char[] finalWord2 = sort(letters);
+        boolean isAnagram = false;
+        if(Arrays.equals(finalWord, finalWord2))
+            isAnagram = true;
+        return isAnagram;
+    }
+
+    public static char[] sort(char[] word) {
+        char temp;
+        for (int ind = 0; ind < word.length; ind++)
+            for (int elm = ind; elm < word.length; elm++)
+                if (word[ind] > word[elm]) {
+                    temp = word[elm];
+                    word[elm] = word[ind];
+                    word[ind] = temp;
+                }
+        return word;
+    }
 }
--- src/Jonathan_Task/MovingZeros.java
+++ src/Jonathan_Task/MovingZeros.java
@@ -1,2 +1,24 @@
-package Jonathan_Task;public class MovingZeros {
+package Jonathan_Task;
+
+import java.util.Arrays;
+
+public class MovingZeros {
+    public static void main(String[] args) {
+        MovingZeros run = new MovingZeros();
+        int[] number = run.moveZeros();
+        System.out.println(Arrays.toString(number));
+
+    }
+
+    public int[] moveZeros() {
+        int[] number = {4, 3, 0, 2, 0, 4, 10, 12};
+        for (int index = 0; index < number.length; index++)
+            for (int num = index + 1; num < number.length; num++)
+                if (number[index] == 0) {
+                    int temp = number[num];
+                    number[num] = number[index];
+                    number[index] = temp;
+                }
+        return number;
+    }
 }
